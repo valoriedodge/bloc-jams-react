2abdf7c2e970f81d9f3ab4ec0b7e3e7cfa6b2639
Valorie <valoriejones@gmail.com>
2018-03-24 21:43:33 -0400

    add album songs

--- src/components/Album.js
+++ src/components/Album.js
@@ -17,7 +17,7 @@ class Album extends Component {
      return (
        <section className="album">
 	 <section id="album-info">
-           <img alt="cover-photo" id="album-cover-art" src={this.state.album.albumCover} />
+           <img alt="cover" id="album-cover-art" src={this.state.album.albumCover} />
            <div className="album-details">
 	     <h1 id="album-title">{this.state.album.title}</h1>
              <h2 className="artist">{this.state.album.artist}</h2>
@@ -31,6 +31,13 @@ class Album extends Component {
              <col id="song-duration-column" />
            </colgroup>  
            <tbody>
+	      {this.state.album.songs.map((song,index)=>
+		<tr key={index}>
+		  <td>{index + 1}</td>
+		  <td>{song.title}</td>
+		  <td>{song.duration}</td>
+		</tr>
+	      )}	
            </tbody>
          </table>
        </section>
